5c1208963c2b2f4b219c8dbc754df530434019c3
daroprito <daroprito_94@hotmail.com>
2014-11-24 13:32:01 -0500

    Nodo2.java
    Clase nodo

---
+++ Nodo2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author David Rodolfo Prieto Torres
+ * @author Erika Ñañez Prada
+ */
+public class Nodo2 {
+        int info; // Almacena dato
+        Nodo2 sig; //Puntero
+    }
